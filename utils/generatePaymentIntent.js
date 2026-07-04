b93ac2888526a3167b3fc7acfc454a384f1f5c30
subhash <subhashsharma931910@gmail.com>
2026-07-04 18:08:01 +0530

    fix: paymentIntents.create typo and variable name mismatch

--- utils/generatePaymentIntent.js
+++ utils/generatePaymentIntent.js
@@ -8,9 +8,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function generatePaymentIntent(orderId, totalPrice) {
   try {
-    const paymentIntern = await stripe.paymentIntent.create({
-      amount: totalPrice * 100, // Convert to cents
-      currency: "usd",
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(totalPrice * 100),
+      currency: "inr",
     });
 
     await database.query(
